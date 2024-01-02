d9572f006dfe9832dd467e28f2b17d80a91dfe86
Phil Chen <06fahchen@gmail.com>
2024-01-02 18:37:49 +0800

    chore: upgrade kysely to 0.27.0 (#7)

--- deps.ts
+++ deps.ts
@@ -13,8 +13,8 @@ export {
   type QueryResult,
   Transaction,
   type TransactionSettings,
-} from "https://esm.sh/kysely@0.26.3";
+} from "https://esm.sh/kysely@0.27.0";
 export { Pool, PoolClient } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
 
-export * as kysely from "https://esm.sh/kysely@0.26.3";
+export * as kysely from "https://esm.sh/kysely@0.27.0";
 export * as postgres from "https://deno.land/x/postgres@v0.17.0/mod.ts";
